903e56b9ffe679509045b9fc175b76d8c0387c68
Sol W <capnsol@gmail.com>
2013-01-22 17:06:20 -0800

    adding week3 hw

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketee::Application.config.secret_token = 'faba020398e5cdaf27a6a25464f78c65a78e71c3edc4d27c3dd3bb619e6ac36278ab569e5faae792ed13c8d49a208fce195747a969b1247a7a22f25d460bc92f'
